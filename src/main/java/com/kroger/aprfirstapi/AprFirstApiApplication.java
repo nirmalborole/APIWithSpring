47f060ee7d32b3d7c638139c0c57edeb0200488b
nirmalborole <nirmalborole1999@gmail.com>
2024-05-29 10:55:41 +0530

    added Api

---
+++ src/main/java/com/kroger/aprfirstapi/AprFirstApiApplication.java
@@ -0,0 +1,13 @@
+package com.kroger.aprfirstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprFirstApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprFirstApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kroger/aprfirstapi/AprFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kroger.aprfirstapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprFirstApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
